2f7840b7fe72425c8ccb4b22a501e39f753c8f85
palich <rautaruukkipalich@gmail.com>
2024-02-20 22:37:30 +0500

    add swagger info

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,17 +5,29 @@ import (
 	"github.com/rautaruukkipalich/go_auth/internal/server"
 )
 
-
 //	@title			Swagger Example API
-//	@version		1.0
-//	@description	This is a sample server Notes
-//	@termsOfService	http://swagger.io/terms/
+//	@version		0.0.1
+//	@description	This is a sample Auth service
 
-// @host		localhost:8080
-// @BasePath	/
+//	@host		localhost:8080
+//	@BasePath	/
+
+//	@SecurityDefinitions.apikey	ApiKeyAuth
+//	@in							header
+//	@name						Authorization
 func main() {
 	config := server.NewConfig()
 	if err := server.Start(config); err != nil {
 		log.Fatal(err)
 	}
 }
+
+// func init() {
+//     // loads values from .env into the system
+//     log.Print("try load env...")
+// 	if err := godotenv.Load(); err != nil {
+//         log.Print("No .env file found")
+// 		return
+//     }
+// 	log.Print("ENV file loaded")
+// }
