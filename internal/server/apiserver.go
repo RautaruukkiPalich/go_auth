605beec573790f2a42fb5d866907f5181a9b956c
palich <rautaruukkipalich@gmail.com>
2024-02-21 20:31:55 +0500

    add migrations on startup

--- internal/server/apiserver.go
+++ internal/server/apiserver.go
@@ -2,40 +2,50 @@ package server
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
-	"github.com/rs/cors"
+	"time"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/rautaruukkipalich/go_auth/internal/store/sqlstore"
 )
 
 func Start(config *Config) error {
 	db, err := newDB(config.DatabaseURL)
-	
 	if err != nil {
 		return err
 	}
-
 	defer db.Close()
 
-	store := sqlstore.New(db)
-	srv := newServer(store)
+	srv := newServer(
+		sqlstore.New(db),
+		config.LogLevel,
+	)
 
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{
-			"http://localhost:8080",
-			"http://127.0.0.1:8080",
-		},
-		AllowedMethods:   []string{
-			http.MethodGet, 
-			http.MethodPost, 
-			http.MethodPatch,
-		},
-		AllowCredentials: true,
-	})
+	if err := migrateTables(db); err != nil {
+		srv.logger.Error(err)
+	}
 
-	handler := c.Handler(srv.router)
+	c := newCors(
+		NewCorsConfig(),
+	)
 
-	return http.ListenAndServe(config.BindAddress, handler)
+	server := &http.Server{
+		Addr: config.BindAddress,
+		Handler: c.Handler(srv.router),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+
+	srv.logger.Info(fmt.Sprintf("server up on '%s'; log level '%s'",
+		server.Addr,
+		srv.logger.Level,
+		),
+	)
+
+	return server.ListenAndServe()
 }
 
 func newDB(databaseURL string) (*sql.DB, error) {
@@ -50,4 +60,25 @@ func newDB(databaseURL string) (*sql.DB, error) {
 	}
 	
 	return db, nil
+}
+
+func migrateTables(db *sql.DB) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"postgres", 
+		driver,
+	)
+	if err != nil {
+		fmt.Println("err :", err)
+		return err
+	}
+
+	m.Up()
+
+	return nil
 }
