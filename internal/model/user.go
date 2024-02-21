fe60d2d065e1d271b9fff7402d6074966c8c93c2
palich <rautaruukkipalich@gmail.com>
2024-02-22 00:28:39 +0500

    add errors

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,7 +3,6 @@ package model
 import (
 	"errors"
 	"regexp"
-	"strings"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
@@ -19,6 +18,13 @@ type User struct {
 	CreatedAt          time.Time `json:"created_at"`
 	UpdatedAt          time.Time `json:"updated_at,omitempty"`
 }
+
+var (
+	ErrorContentUsername = errors.New("use latin letters only")
+	ErrorContentPassword = errors.New("use letters and digits only")
+	ErrorLenUsername = errors.New("username length must be between 3 and 20")
+	ErrorLenPassword = errors.New("password length must be 3 synbols at least")
+)
 
 func (u *User) BeforeCreate() error {
 	if err := u.Validate(); err != nil {
@@ -48,43 +54,32 @@ func (u *User) Validate() error {
 }
 
 func (u *User) ValidateUsername(username string) (err error) {
-	if len(username) < 3 {
-		return errors.New("username must be longer then 3 characters")
-	}
-
-	if len(username) > 20 {
-		return errors.New("username can not be longer then 20 characters")
-	}
-
-	// Valid with regexp
-	matched, err := regexp.MatchString(`^[A-z]+$`, username)
-	if err != nil {
-		return errors.New("server error")
-	}
+	// check username length
+	matched, _ := regexp.MatchString(`^.{3,20}$`, username)
 	if !matched {
-		return errors.New("use latin letters only")
+		return ErrorLenUsername
+	}
+	// check username contains latin letters only
+	matched, _ = regexp.MatchString(`^[A-z]+$`, username)
+	if !matched {
+		return ErrorContentUsername
 	}
 
 	return nil
 }
 
 func (u *User) ValidatePassword(password string) (err error) {
-	if len(password) < 4 {
-		return errors.New("password must be longer then one character")
-	}
-
-	splitPassword := strings.Split(password, " ")
-	if len(splitPassword) > 1 {
-		return errors.New("password can not contains spaces")
-	}
-
-	// Valid with regexp
-	matched, err := regexp.MatchString(`^[\w\d]*$`, password)
-	if err != nil {
-		return errors.New("server error")
-	}
+	
+	// check password length
+	matched, _ := regexp.MatchString(`^.{3,}$`, password)
 	if !matched {
-		return errors.New("use letters and digits only")
+		return ErrorLenPassword
+	}
+
+	// check password contains leters and digits only
+	matched, _ = regexp.MatchString(`^[\w\d]*$`, password)
+	if !matched {
+		return ErrorContentPassword
 	}
 
 	return nil
